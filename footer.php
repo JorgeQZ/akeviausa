2251ed551bc7fa27274b5e203378a70bf4c1f2e4
JorgeQZ <jorge_qzg@hotmail.com>
2024-04-25 11:42:55 -0600

    updates

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <footer class="footer__area">
         <div class="footer__top">
           <div class="container footer-line"></div>
-          <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/akeviaagency4.jpg';?>" alt="Footer Image" data-speed="0.75">
+          <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/brave-advertising.jpg';?>" alt="Footer Image" data-speed="0.75">
         </div>
 
         <div class="footer__btm">
@@ -15,57 +15,34 @@
                     <a href="<?php echo home_url( );?>">
                     <img style="height: 40px;" class="footer__logo" src="<?php echo get_template_directory_uri(  ).'/imgs/logo/logo-akevia.png';?>" alt="Footer Logo">
                     </a>
-                    <p>When do they work well, and when do they on us and finally, when do we actually need how can we
-                      avoid
-                      them.</p>
-                    <!-- <ul class="footer__social">
-                      <li><a href="#"><span><i class="fa-brands fa-facebook-f"></i></span></a></li>
-                      <li><a href="#"><span><i class="fa-brands fa-twitter"></i></span></a></li>
+                    <p>Welcome to our revolution. <br>Brand Revolution®</p>
+                    <ul class="footer__social">
                       <li><a href="#"><span><i class="fa-brands fa-instagram"></i></span></a></li>
-                      <li><a href="#"><span><i class="fa-brands fa-linkedin"></i></span></a></li>
-                    </ul> -->
+                    </ul>
                   </div>
 
                   <div class="footer__widget-2">
-                    <!-- <h2 class="footer__widget-title">Information</h2>
-                    <ul class="footer__link">
-                      <li><a href="about.html">About Company</a></li>
-                      <li><a href="portfolio.html">Case Study</a></li>
-                      <li><a href="career.html">Career</a></li>
-                      <li><a href="blog.html">blog</a></li>
-                      <li><a href="contact.html">contact</a></li>
-                    </ul> -->
+
                   </div>
 
                   <div class="footer__widget-3">
                     <h2 class="footer__widget-title">Contact Us</h2>
                     <ul class="footer__contact">
-                      <!-- <li>Valentin, Street Road 24, New York, USA - 67452</li> -->
-                      <!-- <li><a href="tel:02574328301" class="phone">+02) 574 - 328 - 301 </a></li> -->
+
                       <li><a href="mailto:info@akevia.com">info@akevia.com</a></li>
                     </ul>
                   </div>
 
                   <div class="footer__widget-4">
-                    <h2 class="project-title">Have a project in your mind?</h2>
+                    <h2 class="project-title">Got a project in mind?</h2>
                     <div class="btn_wrapper">
                       <a href="<?php echo get_home_url().'/contact/'; ?>" class="wc-btn-primary btn-hover btn-item"><span></span> Contact Us <i
                           class="fa-solid fa-arrow-right"></i></a>
                     </div>
-                    <h3 class="contact-time">08 : 30 AM - 5 : 30 PM</h3>
-                    <h4 class="contact-day">Monday - Friday</h4>
-                  </div>
 
-                  <div class="footer__copyright">
-                    <p>© 2024 | Alrights reserved by <a href="https://akeviaagency.com/" target="_blank">Akevia</a></p>
                   </div>
+<!--
 
-                  <div class="footer__subscribe">
-                    <!-- <form action="#">
-                      <input type="email" name="email" placeholder="Enter your email">
-                      <button type="submit" class="subs-btn"><i class="fa-solid fa-paper-plane"></i></button>
-                    </form> -->
-                  </div>
                 </div>
               </div>
             </div>
@@ -77,32 +54,6 @@
     </div>
   </div>
 
-
-  <!-- Modal 1 -->
-  <div class="modal__testimonial" id="modal_testimonial">
-    <div class="modal__testimonial-content">
-      <span class="modal__close" id="modal_close"><i class="fa-solid fa-xmark"></i></span>
-      <iframe src="https://www.youtube.com/embed/vZgyWfmw_Kw" title="Testimonial Video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;" allowfullscreen>
-      </iframe>
-    </div>
-  </div>
-
-  <!-- Modal 2 -->
-  <div class="modal__testimonial" id="modal_testimonial2">
-    <div class="modal__testimonial-content">
-      <span class="modal__close" id="modal_close2"><i class="fa-solid fa-xmark"></i></span>
-      <iframe src="https://www.youtube.com/embed/JulIeG1V8T4" allowfullscreen></iframe>
-    </div>
-  </div>
-
-  <!-- Modal 3 -->
-  <div class="modal__testimonial" id="modal_testimonial3">
-    <div class="modal__testimonial-content">
-      <span class="modal__close" id="modal_close3"><i class="fa-solid fa-xmark"></i></span>
-      <iframe src="https://www.youtube.com/embed/7QkL-40FRfE" allowfullscreen></iframe>
-    </div>
-  </div>
 
 
   <!-- All JS files -->
--- frontpage.php
+++ frontpage.php
@@ -17,7 +17,6 @@
 
             <a href="" style="padding-right: 30px;">
               <img src="<?php echo get_template_directory_uri(  ).'/imgs/logo/logo-akevia.png' ?>" alt="Logo Akevia" style="width: 185px;">
-              <!-- <span><i class="fa-solid fa-arrow-right"></i></span> -->
             </a>
             <div class="hero__title-wrapper">
               <h1 class="hero__title">Let's be brave.<br><i>Together</i></h1>
@@ -27,17 +26,15 @@
                 which is what we deliver.
               </p>
             </div>
-            <!-- <img src="<?php echo get_template_directory_uri(  ).'/imgs/icon/arrow-down-big.png';?>" alt="Arrow Down Icon"> -->
-            <!-- <div class="experience">
-              <h2 class="title">Transforming</h2>
-              <p> brands through borderless creativity.</p>
-            </div> -->
           </div>
         </div>
       </div>
     </div>
 
-    <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/nueva-foto-oficina.jpg';?>" alt="image" class="hero1_bg">
+
+    <video class="hero1_bg" autoplay loop muted>
+      <source src="<?php echo get_template_directory_uri(  ).'/video/akevia-bg-video.webm'; ?>">
+    </video>
   </section>
   <!-- Hero area end -->
 
@@ -83,11 +80,11 @@
           <div class="about__content-wrapper">
             <div class="about__img">
               <div class="img-anim">
-                <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/rogelio.jpg';?>" alt="About Image" data-speed="0.3">
+                <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/designing.jpg';?>" alt="About Image" data-speed="0.3">
               </div>
 
               <div class="about__img-right">
-                <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/andrea.jpg';?>" alt="About Image Right" data-speed="0.5">
+                <img src="<?php echo get_template_directory_uri(  ).'/imgs/akevia/hand.jpg';?>" alt="About Image Right" data-speed="0.5">
                 <div class="shape">
                   <div class="secondary" data-speed="0.9"></div>
                   <div class="primary"></div>
@@ -102,11 +99,7 @@
                 Welcome to our revolution. Brand Revolution®.
               </p>
 
-              <!--div class="cursor-btn btn_wrapper">
-                <!-- <a class="btn-item wc-btn-primary btn-hover" href="about.html"><span></span> Explore Us <i
-                    class="fa-solid fa-arrow-right"></i></a> -->
 
-              <!--div-->
             </div>
           </div>
 
@@ -133,14 +126,7 @@
               </p>
           </div>
         </div>
-        <!--div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
-          <div class="service__top-btn">
-            <div class="btn_wrapper">
-              <!-- <a href="service.html" class="btn-item wc-btn-secondary btn-hover"><span></span> View <br>all
-                services <i class="fa-solid fa-arrow-right"></i></a> -->
-            <!--div>
-          </!--div>
-        <!--div-->
+
       </div>
 
       <div class="service__list-wrapper">
@@ -212,378 +198,6 @@
   </section>
   <!-- Service area end -->
 
-
-  <!-- Counter area start -->
-  <!--section class="counter__area">
-    <div class="container g-0 line pt-150">
-      <span class="line-3"></span>
-
-      <div class="row">
-        <div class="col-xxl-12">
-          <div class="counter__wrapper counter_animation">
-            <div class="counter__item counter__anim">
-              <h2 class="counter__number">25k+</h2>
-              <p>Project <br>completed</p>
-              <span class="counter__border"></span>
-            </div>
-            <div class="counter__item counter__anim">
-              <h2 class="counter__number">8k+</h2>
-              <p>Happy <br>customers</p>
-              <span class="counter__border"></span>
-            </div>
-            <div class="counter__item counter__anim">
-              <h2 class="counter__number">15</h2>
-              <p>Years <br>experiences</p>
-              <span class="counter__border"></span>
-            </div>
-            <div class="counter__item counter__anim">
-              <h2 class="counter__number">98</h2>
-              <p>Awards <br>achievement</p>
-              <span class="counter__border"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Counter area end -->
-
-
-  <!-- Workflow area start -->
-  <!--section class="workflow__area">
-    <div class="container g-0 line pt-140 pb-140">
-      <div class="line-3"></div>
-      <div class="row">
-        <div class="col-xxl-12">
-          <div class="sec-title-wrapper">
-            <h2 class="sec-sub-title title-anim">Workflow</h2>
-            <h3 class="sec-title title-anim">How we work</h3>
-          </div>
-        </div>
-
-        <div class="col-xxl-12">
-          <div class="swiper workflow__slider ">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 01</h4>
-                <h5 class="workflow__number">01</h5>
-                <h6 class="workflow__title">Audience</h6>
-                <p>Having these the marketplace to your business</p>
-              </div>
-
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 02</h4>
-                <h5 class="workflow__number">02</h5>
-                <h6 class="workflow__title">Plan & Sketch</h6>
-                <p>Delicate and long-lasting with vitamins and nutritions</p>
-              </div>
-
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 03</h4>
-                <h5 class="workflow__number">03</h5>
-                <h6 class="workflow__title">Customize</h6>
-                <p>Creating brand identities for the digital experiences</p>
-              </div>
-
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 04</h4>
-                <h5 class="workflow__number">04</h5>
-                <h6 class="workflow__title">User Testing</h6>
-                <p>We look forward to engage with beyond the conventional</p>
-              </div>
-
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 05</h4>
-                <h5 class="workflow__number">05</h5>
-                <h6 class="workflow__title">User Testing</h6>
-                <p>We look forward to engage with beyond the conventional</p>
-              </div>
-
-              <div class="swiper-slide workflow__slide fade_left">
-                <h4 class="workflow__step">step 06</h4>
-                <h5 class="workflow__number">06</h5>
-                <h6 class="workflow__title">User Testing</h6>
-                <p>We look forward to engage with beyond the conventional</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Workflow area end -->
-
-
-  <!-- Portfolio area start -->
-  <!--section class="portfolio__area pb-140">
-    <div class="container">
-      <div class="row top_row">
-
-        <h2 class="portfolio__text">work</h2>
-        <div class="portfolio__list-1">
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img class="mover" src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/1.jpg';?>"
-                alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/2.jpg';?>" alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/3.jpg';?>" alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/4.jpg';?>" alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/1.jpg';?>" alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-          <div class="portfolio__item">
-            <a href="portfolio-details.html"><img src="<?php echo get_template_directory_uri(  ).'/imgs/portfolio/1/2.jpg';?>" alt="Portfolio Image"></a>
-            <div class="portfolio__info">
-              <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
-              <p>02 May 2021</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-      <div class="row row_bottom">
-        <div class="col-xxl-12">
-          <div class="portfolio__btn btn_wrapper" data-speed="1" data-lag="0.2">
-            <a class="wc-btn-secondary btn-hover btn-item" href="portfolio.html"><span></span>View <br>all
-              projects <i class="fa-solid fa-arrow-right"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Portfolio area end -->
-
-
-  <!-- Brand area start -->
-  <!--section class="brand__area">
-    <div class="container g-0 line pt-140 pb-130">
-      <span class="line-3"></span>
-      <div class="row">
-        <div class="col-xxl-12">
-          <div class="sec-title-wrapper">
-            <h2 class="sec-sub-title title-anim">Internation Brands</h2>
-            <h3 class="sec-title title-anim">We are happy to work with global <br>
-              largest brands</h3>
-          </div>
-        </div>
-
-        <div class="col-xxl-12">
-          <div class="brand__list">
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/1.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/2.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/3.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/4.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/5.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/6.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/3.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/4.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/5.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/6.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/1.png';?>" alt="Brand Logo">
-            </div>
-            <div class="brand__item fade_bottom">
-              <img src="<?php echo get_template_directory_uri(  ).'/imgs/brand/2.png';?>" alt="Brand Logo">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Brand area end -->
-
-
-  <!-- Testimonial area start -->
-  <!--section class="testimonial__area">
-    <div class="container g-0 line">
-      <span class="line-3"></span>
-      <div class="row">
-        <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-8">
-          <div class="testimonial__wrapper">
-            <div class="testimonial__item item-1">
-              <div class="button modal-trigger">
-                <div class="testimonial__img b-right">
-                  <img src="<?php echo get_template_directory_uri(  ).'/imgs/testimonial/1/1.png';?>" alt="Testimonial Image">
-                </div>
-
-                <h2 class="testimonial__title">Jessica Sherlock</h2>
-                <h3 class="testimonial__role">Manager, Oitaka</h3>
-              </div>
-            </div>
-            <div class="sec-title-wrapper text-anim">
-              <h4 class="sec-sub-title">Testimonials</h4>
-              <h5 class="sec-title title-anim">Clients <br>feedback</h5>
-              <p>Our happy customers give us impactfull and positive feedback on our services, customer supports
-                &
-                etc.</p>
-            </div>
-          </div>
-
-          <div class="testimonial__item item-2">
-            <div class="button modal-trigger-2">
-              <div class="testimonial__img b-left">
-                <img src="<?php echo get_template_directory_uri(  ).'/imgs/testimonial/1/3.png';?>" alt="Testimonial Image">
-              </div>
-
-              <h2 class="testimonial__title">Jessica Sherlock</h2>
-              <h3 class="testimonial__role">Manager, Oitaka</h3>
-            </div>
-          </div>
-        </div>
-        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4">
-          <div class="testimonial__item item-3 img-">
-            <div class="button modal-trigger-3">
-              <div class="testimonial__img b-left">
-                <img src="<?php echo get_template_directory_uri(  ).'/imgs/testimonial/1/2.png';?>" alt="Testimonial Image">
-              </div>
-
-              <h2 class="testimonial__title">adam Smith</h2>
-              <h3 class="testimonial__role">Manager, Oitaka</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Testimonial area end -->
-
-
-  <!-- Blog area start -->
-  <!--section class="blog__area no-pb blog__animation">
-    <div class="container g-0 line pt-150 pb-140">
-      <span class="line-3"></span>
-      <div class="row">
-        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
-          <div class="sec-title-wrapper">
-            <h2 class="sec-sub-title">recent blog</h2>
-            <h3 class="sec-title">News insignt</h3>
-          </div>
-        </div>
-        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4">
-          <article class="blog__item">
-            <div class="blog__img-wrapper">
-              <a href="blog-details.html">
-                <div class="img-box">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/1.jpg';?>" alt="">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/1.jpg';?>" alt="">
-                </div>
-              </a>
-            </div>
-            <h4 class="blog__meta"><a href="category.html">UI Design</a> . 02 May 2019</h4>
-            <h5><a href="blog-details.html" class="blog__title">Ways of lying to yourself about your new
-                relationship.</a></h5>
-            <a href="blog-details.html" class="blog__btn">Read More <span><i
-                  class="fa-solid fa-arrow-right"></i></span></a>
-          </article>
-        </div>
-
-        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4">
-          <article class="blog__item">
-            <div class="blog__img-wrapper">
-              <a href="blog-details.html">
-                <div class="img-box">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/2.jpg';?>" alt="">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/2.jpg';?>" alt="">
-                </div>
-              </a>
-            </div>
-            <h4 class="blog__meta"><a href="category.html">UI Design</a> . 02 May 2019</h4>
-            <h5><a href="blog-details.html" class="blog__title">How to manage a talented and successful de sign
-                team</a></h5>
-            <a href="blog-details.html" class="blog__btn">Read More <span><i
-                  class="fa-solid fa-arrow-right"></i></span></a>
-          </article>
-        </div>
-
-        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4">
-          <article class="blog__item">
-            <div class="blog__img-wrapper">
-              <a href="blog-details.html">
-                <div class="img-box">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/3.jpg';?>" alt="Blog Thumbnail">
-                  <img class="image-box__item" src="<?php echo get_template_directory_uri(  ).'/imgs/blog/1/3.jpg';?>" alt="BLog Thumbnail">
-                </div>
-              </a>
-            </div>
-            <h4 class="blog__meta"><a href="category.html">UI Design</a> . 02 May 2019</h4>
-            <h5><a href="blog-details.html" class="blog__title">How to bring fold to your startup company with
-                Axtra</a></h5>
-            <a href="blog-details.html" class="blog__btn">Read More <span><i
-                  class="fa-solid fa-arrow-right"></i></span></a>
-          </article>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- Blog area end -->
-
-
-  <!-- CTA area start -->
-  <!--section class="cta__area">
-    <div class="container line pb-110">
-      <div class="line-3"></div>
-      <div class="row">
-        <div class="col-xxl-12">
-          <div class="cta__content">
-            <p class="cta__sub-title">Work with us</p>
-            <h2 class="cta__title title-anim">We would love to hear more about your project</h2>
-            <div class="btn_wrapper">
-              <a href="contact.html" class="wc-btn-primary btn-hover btn-item"><span></span>Let’s talk us <i
-                  class="fa-solid fa-arrow-right"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </!--section>
-  <!-- CTA area end -->
 
 </main>
 
